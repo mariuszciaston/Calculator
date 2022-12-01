cf4300dc259b5b00c57acb54c33e4571ae769011
Mariusz <casmar90@gmail.com>
2022-12-01 12:36:57 +0100

    Add functions for operators

---
+++ calc.js
@@ -0,0 +1,25 @@
+// Operators Functions
+
+let a = 10;
+let b = 2;
+
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+};
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
