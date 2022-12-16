348ad07366d09ec5b0f57feed7b5cfbba14246db
Mariusz <casmar90@gmail.com>
2022-12-16 21:21:41 +0100

    Refactor & Fix decimal from keyboard

--- calc.js
+++ calc.js
@@ -1,28 +1,65 @@
-// NUMBERS
+// VALUES
 let selectedDigit = 0;
 let selectedNumber = '';
-
-const numbers = document.querySelectorAll('.num');
-
-let primaryValue = 0;
+let firstNumber = '';
+let secondNumber = '';
+let selectedOperator = '';
+let result = '';
 let secondaryValue = '';
+let primaryValue = 0;
 
+const error = 'cant divide by 0';
+
+// SELECTORS
+const numbers = document.querySelectorAll('.num');
+const decimal = document.querySelector('.decimal');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const backspace = document.querySelector('.backspace');
+const clear = document.querySelector('.clear');
+const buttons = document.querySelectorAll('.btn');
 const secondaryDisplay = document.querySelector('.secondary');
 const primaryDisplay = document.querySelector('.primary');
 
 secondaryDisplay.textContent = secondaryValue;
 primaryDisplay.textContent = primaryValue;
 
-const error = 'cant divide by 0';
-const decimal = document.querySelector('.decimal');
-
+// EVENT LISTENERS
 numbers.forEach((number) => {
-  number.addEventListener('click', (e) => {
-    numberInput(e);
+  number.addEventListener('click', numberInput);
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', (e) => {
+    equalsNow();
+    operatorNow(e);
   });
 });
 
+equals.addEventListener('click', equalsNow);
+backspace.addEventListener('click', backspaceNow);
+clear.addEventListener('click', clearAll);
+
+buttons.forEach((button) => {
+  button.addEventListener('click', resizeFont);
+});
+
+// KEYBOARD SUPPORT
+document.addEventListener('keydown', (e) => {
+  e.preventDefault(); // fixes keyboard input
+  if ((e.key >= '0') && (e.key <= '9') || (e.key === '.')) numberInput(e);
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === 'x' || e.key === '/') operatorNow(e);
+  if (e.key === '=' || e.key === 'Enter') equalsNow();
+  if (e.key === 'Backspace') backspaceNow();
+  if (e.key === 'Escape' || e.key === 'c') clearAll();
+  resizeFont();
+});
+
+// FUNCTIONS
+
+// NUMBERS
 function numberInput(e) {
+
   if (primaryValue == error) {
     clearAll();
   }
@@ -37,68 +74,37 @@ function numberInput(e) {
     if ((primaryValue == '0') && (selectedDigit != '.')) {
       primaryValue = selectedDigit;
     } else {
-      primaryValue += selectedDigit;
+      if (String(primaryValue).includes('.')) {
+        if (selectedDigit !== '.') {
+          primaryValue += selectedDigit;
+        }
+      } else {
+        primaryValue += selectedDigit;
+      }
     }
-
     if (primaryValue == '.') {
       primaryValue = 0 + selectedDigit;
     }
-
-
-
     primaryDisplay.textContent = primaryValue;
-
-
     //
     selectedNumber = primaryValue;
-
     //     firstNumber = primaryValue;
     // secondNumber = '';
-
     // selectedNumber = '';
-
-
     // selectedNumber = primaryValue;
-
     // primaryDisplay.textContent = selectedNumber;
     // secondaryDisplay.textContent = '';
   }
-
-  if (primaryValue.includes('.')) {
-    decimal.disabled = true;
-  } else {
-    decimal.disabled = false;
-  }
 };
 
-
-
-
-
-
 // OPERATORS
-
-let firstNumber = '';
-let selectedOperator = '';
-let secondNumber = '';
-
-const operators = document.querySelectorAll('.operator');
-operators.forEach((operator) => {
-  operator.addEventListener('click', (e) => {
-    equalsNow();
-    operatorNow(e);
-  });
-});
-
 function operatorNow(e) {
-
   if (e.type === 'click') {
     selectedOperator = e.target.textContent;
   }
   if (e.type === 'keydown') {
     selectedOperator = e.key;
-
-    if (e.key == '*') {
+    if (e.key == '*' || e.key == 'x') {
       selectedOperator = 'x';
     }
     if (e.key == '/') {
@@ -106,8 +112,6 @@ function operatorNow(e) {
     }
   }
   primaryValue = primaryDisplay.textContent;
-
-  //  primaryDisplay.textContent = '';
   primaryValue = primaryValue.replace(/\.$|\.0+$/, '');
   firstNumber = primaryValue;
   primaryDisplay.textContent = primaryValue;
@@ -116,11 +120,9 @@ function operatorNow(e) {
   if (selectedOperator) {
     primaryValue = '';
   }
-
 }
 
 // OPERATE
-
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
@@ -138,6 +140,9 @@ function divide(firstNumber, secondNumber) {
     primaryValue = error;
     primaryDisplay.textContent = primaryValue;
 
+    decimal.classList.add('disabled');
+    decimal.disabled = true;
+
     backspace.classList.add('disabled');
     backspace.disabled = true;
 
@@ -145,9 +150,6 @@ function divide(firstNumber, secondNumber) {
       operator.classList.add('disabled');
       operator.disabled = true;
     });
-
-    decimal.classList.add('disabled');
-    decimal.disabled = true;
 
   } else {
     return firstNumber / secondNumber;
@@ -158,34 +160,21 @@ function operate(firstNumber, selectedOperator, secondNumber) {
   if (selectedOperator == '+') {
     return add(firstNumber, secondNumber);
   }
-
   if (selectedOperator == '-') {
     return substract(firstNumber, secondNumber);
   }
-
   if (selectedOperator == 'x') {
     return multiply(firstNumber, secondNumber);
   }
-
   if (selectedOperator == '÷') {
     return divide(firstNumber, secondNumber);
   }
 }
 
 // EQUALS
-
-let result;
-const equals = document.querySelector('.equals');
-
-equals.addEventListener('click', () => {
-  equalsNow();
-});
-
 function equalsNow() {
   secondNumber = selectedNumber;
-
   if ((firstNumber != '') && (selectedOperator != '') && (secondNumber != '')) {
-
     if (primaryValue == error) {
       clearAll();
     } else if (
@@ -201,59 +190,11 @@ function equalsNow() {
       primaryValue = primaryDisplay.textContent;
       firstNumber = result;
       selectedNumber = '';
-
-      if (primaryValue.includes('.')) {
-        decimal.disabled = true;
-      } else {
-        decimal.disabled = false;
-      }
     }
-
   }
 }
 
-// CLEAR
-
-const clear = document.querySelector('.clear');
-
-clear.addEventListener('click', () => {
-  clearAll();
-});
-
-function clearAll() {
-  firstNumber = '';
-  secondNumber = '';
-  selectedOperator = '';
-  selectedDigit = 0;
-  selectedNumber = '';
-  primaryValue = 0;
-  secondaryValue = '';
-
-  result = '';
-  secondaryDisplay.style.fontSize = '24px';
-  primaryDisplay.style.fontSize = '48px';
-  secondaryDisplay.textContent = secondaryValue;
-  primaryDisplay.textContent = primaryValue;
-
-  backspace.classList.remove('disabled');
-  backspace.disabled = false;
-
-  operators.forEach((operator) => {
-    operator.classList.remove('disabled');
-    operator.disabled = false;
-  });
-  decimal.classList.remove('disabled');
-  decimal.disabled = false;
-}
-
 // BACKSPACE
-
-const backspace = document.querySelector('.backspace');
-
-backspace.addEventListener('click', () => {
-  backspaceNow();
-});
-
 function backspaceNow() {
   primaryValue = primaryValue.toString();
   primaryValue = primaryValue.slice(0, -1);
@@ -263,54 +204,60 @@ function backspaceNow() {
   primaryDisplay.textContent = primaryValue;
 };
 
+// CLEAR
+function clearAll() {
+  selectedDigit = 0;
+  selectedNumber = '';
+  firstNumber = '';
+  secondNumber = '';
+  selectedOperator = '';
+  result = '';
+  secondaryValue = '';
+  primaryValue = 0;
+
+  secondaryDisplay.textContent = secondaryValue;
+  primaryDisplay.textContent = primaryValue;
+
+  decimal.classList.remove('disabled');
+  decimal.disabled = false;
+
+  backspace.classList.remove('disabled');
+  backspace.disabled = false;
+
+  operators.forEach((operator) => {
+    operator.classList.remove('disabled');
+    operator.disabled = false;
+  });
+}
+
+// ----------------------------------------------------------------------------------
+
+
 // BUTTON PRESS ANIMATION
-
-const buttons = document.querySelectorAll('.btn');
-
 buttons.forEach((button) => {
 
   button.addEventListener('mousedown', () => {
+
     button.style.animation = 'none';
     button.offsetHeight;
     button.style.animation = 'press 0.2s';
+
     button.classList.add('hold');
+
   });
 
   button.addEventListener('mouseup', () => {
+
     button.classList.remove('hold');
+
     button.style.animation = 'none';
     button.offsetHeight;
     button.style.animation = 'unpress 0.2s';
+
   });
 });
-
-// document.addEventListener('keydown', () => {
-//   // e.preventDefault();
-
-//   button.style.animation = 'none';
-//   button.offsetHeight;
-//   button.style.animation = 'press 0.2s';
-//   button.classList.add('hold');
-// });
-
-// document.addEventListener('keyup', (e) => {
-
-
-//   button.classList.remove('hold');
-//   button.style.animation = 'none';
-//   button.offsetHeight;
-//   button.style.animation = 'unpress 0.2s';
-// });
-
 
 // CHANGE FONT SIZE TO FIT ON DISPLAY
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    resizeFont();
-  });
-});
-
 function resizeFont() {
   if (secondaryDisplay.textContent.length <= 12) {
     secondaryDisplay.style.fontSize = '24px';
@@ -327,7 +274,6 @@ function resizeFont() {
   if (secondaryDisplay.textContent.length >= 16) {
     secondaryDisplay.style.fontSize = '18px';
   }
-
   if (primaryDisplay.textContent.length <= 12) {
     primaryDisplay.style.fontSize = '48px';
   }
@@ -344,38 +290,3 @@ function resizeFont() {
     primaryDisplay.style.fontSize = '36px';
   }
 };
-
-// KEYBOARD SUPPORT
-
-document.addEventListener('keydown', (e) => {
-  e.preventDefault(); // fixes keyboard input
-  if ((e.key >= '0') && (e.key <= '9') || (e.key === '.')) {
-    numberInput(e);
-  }
-
-  if (e.key === '+' || e.key === '-') {
-    operatorNow(e);
-  }
-
-  if (e.key === '*') {
-    operatorNow(e);
-  }
-
-  if (e.key === '/') {
-    operatorNow(e);
-  }
-
-  if (e.key === '=' || e.key === 'Enter') {
-    equalsNow();
-  };
-
-  if (e.key === 'Backspace') {
-    backspaceNow();
-  };
-
-  if (e.key === 'Escape' || e.key === 'c') {
-    clearAll();
-  };
-
-  resizeFont();
-});
